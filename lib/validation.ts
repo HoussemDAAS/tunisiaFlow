079d0f00900df84e18d9f1f52ec518794028b5ca
HoussemDAAS <houssemdaas2@gmail.com>
2024-02-28 11:22:59 +0100

    adding the answer  form

--- lib/validation.ts
+++ lib/validation.ts
@@ -6,3 +6,7 @@ export const QuestionSchema = z.object({
     tags: z.array(z.string().min(1).max(15)).min(1).max(5),
 
   });
+  export const AnswerSchema = z.object({
+   answer :z.string().min(30)
+
+  });
